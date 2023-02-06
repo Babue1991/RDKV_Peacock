d177af4bf2a513245dfedc3c4fdc5f290de68ca5
Babue1991 <babufsp1@gmail.com>
2023-02-06 17:55:17 +0530

    Direct command for screenshot

--- src/test/java/com/automatics/rdkv/commonmethods/CommonMethods.java
+++ src/test/java/com/automatics/rdkv/commonmethods/CommonMethods.java
@@ -23,6 +23,26 @@ public class CommonMethods{
 			e.printStackTrace();
 
 		}
+		
+	
+	}
+	public static void execCommand1(String command) {
+		try {
+
+			Thread.sleep(2000L);
+
+			Process p=Runtime.getRuntime().exec("gst-launch-1.0 v4l2src device=/dev/video1 num-buffers=1 ! jpegenc ! filesink location=/var/lib/jenkins/workspace/RDKV_Peacock/src/test/java/com/automatics/rdkv/liveimage/XfinityHomeScreen.jpg");
+			System.out.println("The terminal command which is executed is : "+command);
+		//	java.io.OutputStream out = p.getOutputStream();  
+
+		//	LOGGER.info("The command executed"+command);
+
+		}catch(Exception e) {
+
+			System.out.println("Error in terminal command execution: "+command);
+			e.printStackTrace();
+
+		}
 	}
 
 	public static void AppButtonHighlight() {
