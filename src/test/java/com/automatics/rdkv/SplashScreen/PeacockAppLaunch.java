15466f62fa3413e5ce3171ecc419142469354f5b
Babue1991 <babufsp1@gmail.com>
2023-02-07 19:31:55 +0530

    test1

--- src/test/java/com/automatics/rdkv/SplashScreen/PeacockAppLaunch.java
+++ src/test/java/com/automatics/rdkv/SplashScreen/PeacockAppLaunch.java
@@ -84,7 +84,7 @@ public class PeacockAppLaunch extends AutomaticsTestBase {
 			//	CommonMethods.execCommand(RemoteKeyContstants.XFINITY_BUTTON);
 				Thread.sleep(5000L);
 				
-				CommonMethods.execCaptureCommand("/var/lib/jenkins/workspace/homescreen1.jpg");
+				CommonMethods.execCaptureCommand("/var/lib/jenkins/workspace/homescreen1.png");
 				
 				Thread.sleep(3000L);
 				
