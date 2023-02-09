5d2f6792fcaefe84baf698abec2838e0bff3f0ed
Babue1991 <babufsp1@gmail.com>
2023-02-09 12:39:45 +0530

    Application screen test case added

--- src/test/java/com/automatics/rdkv/SplashScreen/PeacockAppLaunch.java
+++ src/test/java/com/automatics/rdkv/SplashScreen/PeacockAppLaunch.java
@@ -109,8 +109,8 @@ public class PeacockAppLaunch extends AutomaticsTestBase {
 
 	}
 	
-	@Test(dataProvider = DataProviderConstants.PARALLEL_DATA_PROVIDER, dependsOnGroups = { BroadBandTestGroup.WEBPA }, dataProviderClass = AutomaticsTapApi.class, dependsOnMethods= {"testVerifySplashScreenSTB"}, alwaysRun = true, enabled = true, groups = {
-			BroadBandTestGroup.NEW_FEATURE, BroadBandTestGroup.WEBPA})
+	@Test(dataProvider = DataProviderConstants.PARALLEL_DATA_PROVIDER, dataProviderClass = AutomaticsTapApi.class, alwaysRun = true, enabled = true, groups = {
+		BroadBandTestGroup.NEW_FEATURE, BroadBandTestGroup.WEBPA, "AppLaunch"  })
 	@TestDetails(testUID = "PEACOCK-AAMP-TC-1002")
 	public void testVerifyApplicationScreen(Dut device) throws InterruptedException {
 		// Variables declaration starts
