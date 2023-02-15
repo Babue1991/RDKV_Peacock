449728a42bcfe697b9c363d306078909fbb83940
Babue1991 <babufsp1@gmail.com>
2023-02-15 10:38:52 +0530

    Test case modified

--- src/test/java/com/automatics/rdkv/commonmethods/GrabText.java
+++ src/test/java/com/automatics/rdkv/commonmethods/GrabText.java
@@ -8,12 +8,13 @@ import javax.imageio.ImageIO;
 
 import com.automatics.rdkv.constants.ImageCaptureConstants;
 import com.automatics.rdkv.constants.LanguageConstants;
+import com.automatics.test.AutomaticsTestBase;
 
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
-public class GrabText {
+public class GrabText extends AutomaticsTestBase{
 
 	public static void main(String[] args) throws IOException {
 		String path="/home/user/Downloads/Application.jpg";
@@ -23,19 +24,20 @@ public class GrabText {
 	}
 
 	public String crackImage(BufferedImage filePath) {
-
+		String text;
+		String result;
 		ITesseract instance = new Tesseract();
-	//	instance.setDatapath(LanguageConstants.TRAIN_DATA_PATH);
-	//	instance.setLanguage(LanguageConstants.ENGLISH_LANGUAGE);
-
 		instance.setDatapath(LanguageConstants.TRAIN_DATA_PATH);
-		instance.setLanguage("eng");
+		instance.setLanguage(LanguageConstants.ENGLISH_LANGUAGE);
+
+		//instance.setDatapath(LanguageConstants.TRAIN_DATA_PATH);
+		//instance.setLanguage("eng");
 		
 		
 		try {
-			String text = instance.doOCR(filePath);
-			String result = text.trim();
-			System.out.println("The text present in the image is:"+result+"apps");
+			text = instance.doOCR(filePath);
+			result = text.trim();
+			LOGGER.info("The grabbed text from image is: " +result);
 			return result;
 		} catch (TesseractException e) {
 			System.err.println(e.getMessage());
