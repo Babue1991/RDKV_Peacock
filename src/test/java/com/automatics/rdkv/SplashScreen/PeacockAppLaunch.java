8261fdad6de1e800e36b76f8fe79dae930c6c439
Babue1991 <babufsp1@gmail.com>
2023-02-15 12:57:28 +0530

    Test case modified

--- src/test/java/com/automatics/rdkv/SplashScreen/PeacockAppLaunch.java
+++ src/test/java/com/automatics/rdkv/SplashScreen/PeacockAppLaunch.java
@@ -322,7 +322,7 @@ public class PeacockAppLaunch extends AutomaticsTestBase {
 			liveImage = ImageIO.read(new File(ImageCaptureConstants.XFINITY_APPLICATION_FOCUS_PEACOCK));
 
 			LOGGER.info("Crop the live image");
-			subImage = CropImage.subImage(liveImage, 50,300,160,45);
+			subImage = CropImage.subImage(liveImage, 50,250,160,45);
 
 			File outputFile = new File("/var/lib/jenkins/workspace/image1.jpg");
 			ImageIO.write(subImage, "jpg", outputFile);
