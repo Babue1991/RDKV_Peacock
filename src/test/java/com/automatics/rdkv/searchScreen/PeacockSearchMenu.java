dce7219aae121342de46a147156d41ffd8d5638b
Babue1991 <babufsp1@gmail.com>
2023-02-16 17:47:25 +0530

    Added pixel values for movie screen

--- src/test/java/com/automatics/rdkv/searchScreen/PeacockSearchMenu.java
+++ src/test/java/com/automatics/rdkv/searchScreen/PeacockSearchMenu.java
@@ -50,7 +50,7 @@ public class PeacockSearchMenu extends AutomaticsTestBase {
 	@Test(priority=6, dataProvider = DataProviderConstants.PARALLEL_DATA_PROVIDER, dataProviderClass = AutomaticsTapApi.class, alwaysRun = true, enabled = true, groups = {
 			BroadBandTestGroup.NEW_FEATURE, BroadBandTestGroup.WEBPA, "AppLaunch"  })
 	@TestDetails(testUID = "PEACOCK-AAMP-TC-1006")
-	public void testVerifySplashScreenSTB(Dut device) throws InterruptedException {
+	public void testVerifySearchMenu(Dut device) throws InterruptedException {
 		// Variables declaration starts
 		boolean status = false;
 		String testId = "PEACOCK-AAMP-TC-106";
@@ -123,5 +123,83 @@ public class PeacockSearchMenu extends AutomaticsTestBase {
 
 		}
 		LOGGER.info("ENDING TEST CASE: PEACOCK-AAMP-TC-1006");
+	}
+	@Test(priority=7,dataProvider = DataProviderConstants.PARALLEL_DATA_PROVIDER, dataProviderClass = AutomaticsTapApi.class, alwaysRun = true, enabled = true, groups = {
+			BroadBandTestGroup.NEW_FEATURE, BroadBandTestGroup.WEBPA, "AppLaunch"  })
+	@TestDetails(testUID = "PEACOCK-AAMP-TC-1007")
+	public void testVerifySearchScreenKeyboard(Dut device) throws InterruptedException {
+		// Variables declaration starts
+		boolean status = false;
+		String testId = "PEACOCK-AAMP-TC-107";
+		String errorMessage = null;
+		String stepNum = null;
+		Mat referenceImage;
+		BufferedImage liveImage;
+		BufferedImage subImage;
+		BufferedImage cropImage;
+		// Variables declaration Ends
+
+		LOGGER.info("#######################################################################################");
+		LOGGER.info("STARTING TEST CASE: PEACOCK-AAMP-TC-1007");
+		LOGGER.info("TEST DESCRIPTION: This test is to verify search bar screen ");
+		LOGGER.info("TEST STEPS : ");
+		LOGGER.info("1.Launch peacock home screen and press Left_button and press up ");
+		LOGGER.info("#######################################################################################");
+		try {
+			stepNum = "S1";
+			errorMessage = "Failed to get the search icon";
+			LOGGER.info("*****************************************************************************************");
+			LOGGER.info("STEP 1: DESCRIPTION : This test is to compare search bar reference image with the live image");
+			LOGGER.info("STEP 1: ACTION : ACTION: compare search bar reference image with the live image ");
+			LOGGER.info("STEP 1: EXPECTED : Image comparison successful.");
+			LOGGER.info("*****************************************************************************************");
+
+			LOGGER.info("Crop the reference image from the search bar menu");
+			
+			LOGGER.info("Click one LEFT_BUTTON ");
+			CommonMethods.execCommand(RemoteKeyContstants.LEFT_BUTTON);
+			
+			LOGGER.info("Click one UP_BUTTON ");
+			CommonMethods.execCommand(RemoteKeyContstants.UP_BUTTON);
+			
+			LOGGER.info("Capture peacock search screen live image ");
+			CaptureLiveImage.capture(ImageCaptureConstants.PEACOCK_SEARCH_BAR_LIVE_IMAGE);	
+			
+			LOGGER.info("Reading live image");
+			liveImage = ImageIO.read(new File(ImageCaptureConstants.PEACOCK_SEARCH_BAR_LIVE_IMAGE));
+			
+			LOGGER.info("Croping the live search keyboard image");
+		    subImage = CropImage.cropImage(liveImage, 80,100,1180,180);
+			
+			LOGGER.info(" ");
+			nu.pattern.OpenCV.loadLocally();
+			
+			LOGGER.info("Reading reference image");
+			referenceImage = Imgcodecs.imread(ImageCaptureConstants.PEACOCK_SEARCH_BAR);
+			
+			ImageCompare imgCompare =new ImageCompare();
+			
+			LOGGER.info("Calling screen compare method");
+		
+			if (status) {
+				LOGGER.info("The status of image comparision is: " + status);
+			} else {
+				LOGGER.error("STEP 1: ACTUAL : " + errorMessage);
+			}
+			LOGGER.info("**********************************************************************************");
+			tapEnv.updateExecutionStatus(device, testId, stepNum, status, errorMessage, false);
+
+		} catch (Exception e) {
+			LOGGER.error("Exception occured while reading the image file " + e);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			LOGGER.info("Inside catch");
+			errorMessage = e.getMessage();
+			LOGGER.error("Exception while launching search screen file: " + errorMessage);
+			CommonUtils.updateTestStatusDuringException(tapEnv, device, testId, stepNum, status, errorMessage, false);
+
+		}
+		LOGGER.info("ENDING TEST CASE: PEACOCK-AAMP-TC-1007");
+		
 	}
 }
