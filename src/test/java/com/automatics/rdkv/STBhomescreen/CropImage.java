9f88ae7e4b06156d147c93e2bc25e35cc5850a81
Babue1991 <babufsp1@gmail.com>
2023-02-22 11:25:59 +0530

    wwe changes

--- src/test/java/com/automatics/rdkv/STBhomescreen/CropImage.java
+++ src/test/java/com/automatics/rdkv/STBhomescreen/CropImage.java
@@ -19,7 +19,7 @@ public class CropImage {
 		BufferedImage liveImage = ImageIO.read(new File("/home/user/Desktop/SearchButttonHighlight.jpg"));
 		
 		try {
-			cropedImage = cropImage(liveImage, 40,170,240,60);
+			cropedImage = cropImage(liveImage, 50,300,160,45);
 			
 			//Popular entertainment section  70,380,380,70
 			
