453881cb0c3f5381333264498efed3f4bf923bb5
Babitha <Babitha.c@ltts.com>
2023-02-28 11:59:31 +0530

    search

--- src/test/java/com/automatics/rdkv/constants/ImageCaptureConstants.java
+++ src/test/java/com/automatics/rdkv/constants/ImageCaptureConstants.java
@@ -197,6 +197,12 @@ public class ImageCaptureConstants {
 	
 	/********The variable used to send capture card port to capture Search Section Screen image**/
 	public static String PEACOCK_SEARCH_SUBTITLE=LIVE_IMAGE_PATH+"SearchAntzPause.jpg";
+	
+	/********The variable used to send capture card port to capture Peacock search Feature Antz image**/
+	public static String PEACOCK_LEFTMENU_REFERENCE_IMAGE=REFERENCE_IMAGE_PATH+"SearchPlayReference.jpg";
+	
+	/********The variable used to send capture card port to capture Search Section Screen image**/
+	public static String PEACOCK_LEFTMENU_LIVE_IMAGE=LIVE_IMAGE_PATH+"SearchAntzPause.jpg";
 	//Search
 	
 	/********The variable used to send capture card port to capture Peacock menu movies option image**/
--- src/test/java/com/automatics/rdkv/searchScreen/PeacockSearchMenu.java
+++ src/test/java/com/automatics/rdkv/searchScreen/PeacockSearchMenu.java
@@ -49,6 +49,81 @@ public class PeacockSearchMenu extends AutomaticsTestBase {
 	
 
 	static Process p;
+	@Test(priority=5,dataProvider = DataProviderConstants.PARALLEL_DATA_PROVIDER, dataProviderClass = AutomaticsTapApi.class, alwaysRun = true, enabled = true, groups = {
+			BroadBandTestGroup.NEW_FEATURE, BroadBandTestGroup.WEBPA, "AppLaunch"  })
+	@TestDetails(testUID = "PEACOCK-AAMP-TC-1005")
+	public void testVerifyPeacockMenu(Dut device) throws InterruptedException {
+		// Variables declaration starts
+		boolean status = false;
+		String testId = "PEACOCK-AAMP-TC-005";
+		String errorMessage = null;
+		String stepNum = null;
+		BufferedImage referenceImage;
+		BufferedImage liveImage;
+		BufferedImage subImage;
+		// Variables declaration Ends
+
+		LOGGER.info("#######################################################################################");
+		LOGGER.info("STARTING TEST CASE: PEACOCK-AAMP-TC-1005");
+		LOGGER.info("TEST DESCRIPTION: This test is to verify user can navigate to Peacock left menu using remote keys");
+		LOGGER.info("TEST STEPS : ");
+		LOGGER.info("1. Press left button for the Peacock left menu");
+		LOGGER.info("#######################################################################################");
+		try {
+			stepNum = "S1";
+			errorMessage = "Failed to navigate to Peacock menu section";
+			LOGGER.info("*****************************************************************************************");
+			LOGGER.info("STEP 1: DESCRIPTION : This test is to verify user can naviagte to Peacock menu using remote keys");
+			LOGGER.info("STEP 1: ACTION : Press left button and do screen capture");
+			LOGGER.info("STEP 1: EXPECTED :Peacock menu section should launch successfully.");
+			LOGGER.info("*****************************************************************************************");
+
+			LOGGER.info("Redirecting to Peacock menu screen: ");
+			LOGGER.info("Click Xfinity left button ");
+			CommonMethods.execCommand(RemoteKeyContstants.LEFT_BUTTON);
+			Thread.sleep(5000L);
+			nu.pattern.OpenCV.loadLocally();
+			
+			LOGGER.info("Reading reference image");
+			 referenceImage = ImageIO.read(new File(ImageCaptureConstants.PEACOCK_LEFTMENU_REFERENCE_IMAGE));
+
+			LOGGER.info("Capture application screen live image");
+			CaptureLiveImage.capture(ImageCaptureConstants.PEACOCK_LEFTMENU_LIVE_IMAGE);
+			Thread.sleep(5000L);
+			
+			LOGGER.info("Reading live image");
+			liveImage = ImageIO.read(new File(ImageCaptureConstants.PEACOCK_LEFTMENU_LIVE_IMAGE));
+			
+			LOGGER.info("Calling Crop Image method");
+			subImage = CropImage.cropImage(liveImage, 35,150,250,420);
+			ImageCompare imgCompare =new ImageCompare();
+			
+			LOGGER.info("Calling screen compare method");
+			status = imgCompare.compare(referenceImage, subImage);
+			
+			if (status) {
+				LOGGER.info("The status of image comparision is: " + status);
+			} else {
+				LOGGER.error("STEP 1: ACTUAL : " + errorMessage);
+			}
+			LOGGER.info("**********************************************************************************");
+			tapEnv.updateExecutionStatus(device, testId, stepNum, status, errorMessage, false);
+
+		} catch (Exception e) {
+			LOGGER.error("Exception occured while reading the image file " + e);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			LOGGER.info("Inside catch");
+			errorMessage = e.getMessage();
+			LOGGER.error("Exception while launching movie screen file: " + errorMessage);
+			CommonUtils.updateTestStatusDuringException(tapEnv, device, testId, stepNum, status, errorMessage, false);
+
+		}
+		LOGGER.info("ENDING TEST CASE: TC-RDKV-STB-1005");
+
+	}
+	
+	
 	@Test(priority=6, dataProvider = DataProviderConstants.PARALLEL_DATA_PROVIDER, dataProviderClass = AutomaticsTapApi.class, alwaysRun = true, enabled = true, groups = {
 			BroadBandTestGroup.NEW_FEATURE, BroadBandTestGroup.WEBPA, "AppLaunch"  })
 	@TestDetails(testUID = "PEACOCK-AAMP-TC-1006")
