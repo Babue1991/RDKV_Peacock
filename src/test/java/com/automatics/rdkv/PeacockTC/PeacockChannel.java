836e9abbf4d311979fd43af1b628bf775d9f47aa
Babue1991 <babufsp1@gmail.com>
2023-03-13 15:02:38 +0530

    code changes

--- src/test/java/com/automatics/rdkv/PeacockTC/PeacockChannel.java
+++ src/test/java/com/automatics/rdkv/PeacockTC/PeacockChannel.java
@@ -866,7 +866,7 @@ public class PeacockChannel extends AutomaticsTestBase {
 			LOGGER.info("Click Xfinity down button");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
 			
-			Thread.sleep(3000);
+			Thread.sleep(2000);
 			
 			LOGGER.info("Capture application screen live image");
 			CaptureLiveImage.captureIcon(ImageCaptureConstants.PEACOCK_LINEAR_CHANNEL_EPISODES);
