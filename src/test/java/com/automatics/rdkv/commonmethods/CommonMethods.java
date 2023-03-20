8a0a39ea5dfb29e9ed7b7d051bc85fa86ff294bc
Babue1991 <babufsp1@gmail.com>
2023-03-20 15:07:27 +0530

    code changes

--- src/test/java/com/automatics/rdkv/commonmethods/CommonMethods.java
+++ src/test/java/com/automatics/rdkv/commonmethods/CommonMethods.java
@@ -367,18 +367,28 @@ public class CommonMethods extends AutomaticsTestBase{
 		
     	LOGGER.info("Click Xfinity Down button ");
 		CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
+		
+		Thread.sleep(1000L);
 	    
 	    LOGGER.info("Click Xfinity up button ");
 	    CommonMethods.execCommandIcon(RemoteKeyContstants.UP_BUTTON);
+	    
+	    Thread.sleep(1000L);
 		
 		LOGGER.info("Click Xfinity left button ");
 		CommonMethods.execCommandIcon(RemoteKeyContstants.LEFT_BUTTON);
 		
+		Thread.sleep(1000L);
+		
 		LOGGER.info("Click Xfinity OK button ");
 		CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
 		
+		Thread.sleep(1000L);
+		
 		LOGGER.info("Click Xfinity right button ");
 		CommonMethods.execCommandIcon(RemoteKeyContstants.RIGHT_BUTTON);
+		
+		Thread.sleep(1000L);
 		
 		LOGGER.info("Click Xfinity OK button ");
 		CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
@@ -391,17 +401,22 @@ public class CommonMethods extends AutomaticsTestBase{
     	LOGGER.info("Click Xfinity OK button ");
  	    CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
  	    
+ 	   Thread.sleep(1000L);
+ 	    
  		LOGGER.info("Click Xfinity left button ");
  		CommonMethods.execCommandIcon(RemoteKeyContstants.LEFT_BUTTON);
+ 		Thread.sleep(1000L);
  		
  		LOGGER.info("Click Xfinity left button ");
  		CommonMethods.execCommandIcon(RemoteKeyContstants.LEFT_BUTTON);
- 		
+ 		Thread.sleep(1000L);
  		LOGGER.info("Click Xfinity OK button ");
  		CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
+ 		Thread.sleep(1000L);
  		
  		LOGGER.info("Click Xfinity up button ");
 		CommonMethods.execCommandIcon(RemoteKeyContstants.UP_BUTTON);
+		Thread.sleep(1000L);
 		
 		LOGGER.info("Click Xfinity OK button ");
  		CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
