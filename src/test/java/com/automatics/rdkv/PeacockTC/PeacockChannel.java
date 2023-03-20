104e533de92fbc0c125fc2a9c28b671d205a82b2
Babue1991 <babufsp1@gmail.com>
2023-03-20 16:55:55 +0530

    code changes

--- src/test/java/com/automatics/rdkv/PeacockTC/PeacockChannel.java
+++ src/test/java/com/automatics/rdkv/PeacockTC/PeacockChannel.java
@@ -68,17 +68,17 @@ public class PeacockChannel extends AutomaticsTestBase {
 			LOGGER.info("STEP 1: ACTION :Enable subtitle and then verify text");
 			LOGGER.info("STEP 1: EXPECTED : Subtitle should displayed");
 			LOGGER.info("*****************************************************************************************");
-			
+
 			LOGGER.info("Calling method to navigate to linear channel ");
 			CommonMethods.navigateToChannelFour();
-			
+
 			Thread.sleep(2000);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
 
 			Thread.sleep(20000);
-			
+
 			LOGGER.info("Calling disable subtitle to make sure the subtitle is off ");
 			CommonMethods.disableChannelSubtitle();
 
@@ -137,14 +137,14 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling method to navigate to linear channel ");
 			CommonMethods.navigateToChannelFour();
-			
+
 			Thread.sleep(2000);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
 
 			Thread.sleep(20000);
-			
+
 			LOGGER.info("Calling disable subtitle to make sure the subtitle is off ");
 			CommonMethods.disableChannelSubtitle();
 
@@ -239,14 +239,14 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling method to navigate to linear channel ");
 			CommonMethods.navigateToChannelFour();
-			
+
 			Thread.sleep(2000);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
 
 			Thread.sleep(20000);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
 
@@ -352,14 +352,14 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling method to navigate to linear channel ");
 			CommonMethods.navigateToChannelFour();
-			
+
 			Thread.sleep(2000);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
 
 			Thread.sleep(20000);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
 
@@ -772,9 +772,9 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling method to navigate to linear channels");
 			CommonMethods.navigateToChannelFour();
-			
+
 			Thread.sleep(3000);
-			
+
 			LOGGER.info("Capture application screen live image");
 			CaptureLiveImage.captureIcon(ImageCaptureConstants.PEACOCK_LINEAR_CHANNEL_EPISODES);
 
@@ -783,25 +783,25 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling image cropping method");
 			subImage = CropImage.cropImage(liveImage, 210,395,425,50);
-			
+
 			LOGGER.info("Calling method to read text in image");
 			GrabText grabText = new GrabText();
 			expected = grabText.crackImage(subImage);
-			
+
 			Thread.sleep(5000);
-			
+
 			LOGGER.info("Click Xfinity down button");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity down button");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity down button");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity down button");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity right button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.RIGHT_BUTTON);
 
@@ -812,21 +812,21 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity left button twice");
 			CommonMethods.execCommandRepeat2(RemoteKeyContstants.LEFT_BUTTON,2);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
-			
+
 			TimeUnit. MINUTES. sleep(2);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity Down button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
-			
+
 			LOGGER.info("Capture application screen live image");
 			CaptureLiveImage.captureIcon(ImageCaptureConstants.PEACOCK_LINEAR_CHANNEL_PREVIOUS_EPISODE);
 
@@ -835,7 +835,7 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling image cropping method");
 			subImage = CropImage.cropImage(liveImage, 210,395,425,50);
-			
+
 			File outputFile = new File("/var/lib/jenkins/workspace/preepisode.jpg");
 			ImageIO.write(subImage, "jpg", outputFile);
 
@@ -849,7 +849,7 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling method to read text in image");
 			actual = grabText.crackImage(greyImage);
-			
+
 			LOGGER.info("Calling method to compare text in image");
 
 			status = CommonMethods.partialTextCompare(expected, actual);
@@ -871,11 +871,11 @@ public class PeacockChannel extends AutomaticsTestBase {
 			CommonUtils.updateTestStatusDuringException(tapEnv, device, testId, stepNum, status, errorMessage, false);
 
 		}
-		
+
 		LOGGER.info("ENDING TEST CASE: TC-RDKV-STB-4026");
 
 	}
-	
+
 	@Test(priority=8,dataProvider = DataProviderConstants.PARALLEL_DATA_PROVIDER, dataProviderClass = AutomaticsTapApi.class, alwaysRun = true, enabled = true, groups = {
 			BroadBandTestGroup.NEW_FEATURE, BroadBandTestGroup.WEBPA, "AppLaunch"  })
 	@TestDetails(testUID = "PEACOCK-AAMP-TC-4028")
@@ -909,9 +909,9 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling method to navigate to linear channels");
 			CommonMethods.navigateToChannelFour();
-			
+
 			Thread.sleep(2000);
-			
+
 			LOGGER.info("Capture application screen live image");
 			CaptureLiveImage.captureIcon(ImageCaptureConstants.PLCHANNEL_EPISODES);
 
@@ -920,52 +920,52 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling image cropping method");
 			subImage = CropImage.cropImage(liveImage, 820,395,425,60);
-			
+
 			LOGGER.info("Calling method to read text in image");
 			GrabText grabText = new GrabText();
 			expected = grabText.crackImage(subImage);
-			
-            Thread.sleep(5000);
-			
+
+			Thread.sleep(5000);
+
 			LOGGER.info("Click Xfinity down button");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity down button");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity down button");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity down button");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
 
 			Thread.sleep(20000);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity right button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.RIGHT_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity right button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.RIGHT_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
-			
+
 			TimeUnit. MINUTES. sleep(2);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
-			
+
 			LOGGER.info("Click Xfinity Down button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.DOWN_BUTTON);
-			
+
 			Thread.sleep(2000);
-			
+
 			LOGGER.info("Capture application screen live image");
 			CaptureLiveImage.captureIcon(ImageCaptureConstants.PEACOCK_LINEAR_CHANNEL_NEXT_EPISODE);
 
@@ -974,7 +974,7 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling image cropping method");
 			subImage = CropImage.cropImage(liveImage, 450,395,475,50);
-			
+
 			File outputFile = new File("/var/lib/jenkins/workspace/nextepisode.jpg");
 			ImageIO.write(subImage, "jpg", outputFile);
 
@@ -988,7 +988,7 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling method to read text in image");
 			actual = grabText.crackImage(greyImage);
-			
+
 			LOGGER.info("Calling method to compare text in image");
 
 			status = CommonMethods.partialTextCompare(expected, actual);
@@ -1011,7 +1011,7 @@ public class PeacockChannel extends AutomaticsTestBase {
 			CommonUtils.updateTestStatusDuringException(tapEnv, device, testId, stepNum, status, errorMessage, false);
 
 		}
-		
+
 		LOGGER.info("ENDING TEST CASE: TC-RDKV-STB-4028");
 
 	}
@@ -1026,7 +1026,7 @@ public class PeacockChannel extends AutomaticsTestBase {
 		String stepNum = null;
 		BufferedImage liveImage;
 		BufferedImage referenceImage;
-		
+
 		// Variables declaration Ends
 
 		LOGGER.info("#######################################################################################");
@@ -1043,17 +1043,17 @@ public class PeacockChannel extends AutomaticsTestBase {
 			LOGGER.info("STEP 1: ACTION :Tune to  linear channels  which has ads");
 			LOGGER.info("STEP 1: EXPECTED : FFWD/RWD functionalities should not work when Ad is in progress");
 			LOGGER.info("*****************************************************************************************");
-            
+
 			LOGGER.info("Calling method to navigate to linear channels");
 			CommonMethods.navigateToChannelFour();
-			
+
 			Thread.sleep(2000);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
-			
+
 			Thread.sleep(20000);
-			
+
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
 
@@ -1078,17 +1078,17 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
-			
+
 			Thread.sleep(3000);
-			
+
 			LOGGER.info("Capture application screen live image");
 			CaptureLiveImage.capture(ImageCaptureConstants.PLC_ADS_TP_PAUSE_REF);
 
 			LOGGER.info("Reading first live ads screen");
 			referenceImage = ImageIO.read(new File(ImageCaptureConstants.PLC_ADS_TP_PAUSE_REF));
-			
+
 			Thread.sleep(5000);
-			
+
 			LOGGER.info("Capture application screen live image");
 			CaptureLiveImage.captureIcon(ImageCaptureConstants.PLC_ADS_TP_PAUSE_LIVE);
 
@@ -1096,8 +1096,8 @@ public class PeacockChannel extends AutomaticsTestBase {
 			liveImage = ImageIO.read(new File(ImageCaptureConstants.PLC_ADS_TP_PAUSE_LIVE));
 
 			LOGGER.info("Calling image compare method");
-			
-            ImageCompare imgCompare =new ImageCompare();
+
+			ImageCompare imgCompare =new ImageCompare();
 
 			status = imgCompare.compare(referenceImage, liveImage);
 
@@ -1119,7 +1119,7 @@ public class PeacockChannel extends AutomaticsTestBase {
 			CommonUtils.updateTestStatusDuringException(tapEnv, device, testId, stepNum, status, errorMessage, false);
 
 		}
-		
+
 		LOGGER.info("ENDING TEST CASE: TC-RDKV-STB-4024");
 
 	}
@@ -1148,12 +1148,12 @@ public class PeacockChannel extends AutomaticsTestBase {
 			LOGGER.info("STEP 1: ACTION :Enable subtitle and then verify text");
 			LOGGER.info("STEP 1: EXPECTED : Subtitle should displayed");
 			LOGGER.info("*****************************************************************************************");
-			
+
 			LOGGER.info("Calling method to navigate to linear channel ");
 			CommonMethods.navigateToSLE();
-			
+
 			TimeUnit. MINUTES. sleep(2);
-			
+
 			LOGGER.info("Calling disable subtitle to make sure the subtitle is off ");
 			CommonMethods.disableSLEAssetSubtitle();
 
@@ -1214,26 +1214,26 @@ public class PeacockChannel extends AutomaticsTestBase {
 			LOGGER.info("STEP 1: ACTION : Perform Rewind operation at until Live edge indicator is off");
 			LOGGER.info("STEP 1: EXPECTED : Go Live icon should be enabled");
 			LOGGER.info("*****************************************************************************************");
-			
+
 			LOGGER.info("Calling method to navigate to linear channel ");
 			CommonMethods.navigateToSLE();
-			
+
 			TimeUnit. MINUTES. sleep(2);
-			
+
 			LOGGER.info("Calling rewind method ");
 			CommonMethods.rewindSLE();
-			
+
 			TimeUnit. MINUTES. sleep(1);
-			
-			
+
+
 			LOGGER.info("Click Xfinity up button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.UP_BUTTON);
-			
+
 			Thread.sleep(2000L);
-			
+
 			LOGGER.info("Click Xfinity up button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.UP_BUTTON);
-			
+
 			Thread.sleep(2000L);
 
 			LOGGER.info("Capture application screen live image");
@@ -1244,7 +1244,7 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling image cropping method");
 			subImage = CropImage.cropImage(liveImage,  98,575,67,35);
-			
+
 			File outputFile = new File("/var/lib/jenkins/workspace/golive.jpg");
 			ImageIO.write(subImage, "jpg", outputFile);
 
@@ -1255,13 +1255,13 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			File outputFiletwo = new File("/var/lib/jenkins/workspace/greygolive.jpg");
 			ImageIO.write(greyImage, "jpg", outputFiletwo);
-			
+
 			BufferedImage greyImageOutput = ImageIO.read(new File("/var/lib/jenkins/workspace/greygolive.jpg"));
 
 			LOGGER.info("Calling method to read text in image");
 			GrabText grabText = new GrabText();
 			actual = grabText.crackImage(greyImageOutput);
-			
+
 			LOGGER.info("Calling method to compare text in image");
 
 			status = CommonMethods.partialTextCompare(expected, actual);
@@ -1290,17 +1290,17 @@ public class PeacockChannel extends AutomaticsTestBase {
 			LOGGER.info("STEP 1: ACTION : Seek to Live edge position by pressing on the Go Live icon near subtitles");
 			LOGGER.info("STEP 1: EXPECTED :Live icon near title should be enabled");
 			LOGGER.info("*****************************************************************************************");
-			
+
 			TimeUnit. MINUTES. sleep(2);
-			
+
 			LOGGER.info("Click Xfinity up button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.UP_BUTTON);
-			
+
 			Thread.sleep(2000L);
-			
+
 			LOGGER.info("Click Xfinity ok button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
-			
+
 			Thread.sleep(2000L);
 
 			LOGGER.info("Capture application screen live image");
@@ -1311,7 +1311,7 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			LOGGER.info("Calling image cropping method");
 			subImage = CropImage.cropImage(liveImage,  598,65,45,20);
-			
+
 			File outputFile = new File("/var/lib/jenkins/workspace/slelive.jpg");
 			ImageIO.write(subImage, "jpg", outputFile);
 
@@ -1322,13 +1322,13 @@ public class PeacockChannel extends AutomaticsTestBase {
 
 			File outputFiletwo = new File("/var/lib/jenkins/workspace/greylive.jpg");
 			ImageIO.write(greyImage, "jpg", outputFiletwo);
-			
+
 			BufferedImage greyImageOutput = ImageIO.read(new File("/var/lib/jenkins/workspace/greylive.jpg"));
 
 			LOGGER.info("Calling method to read text in image");
 			GrabText grabText = new GrabText();
 			actual = grabText.crackImage(greyImageOutput);
-			
+
 			LOGGER.info("Calling method to compare text in image");
 
 			status = CommonMethods.partialTextCompare(expectedLive, actual);
@@ -1352,6 +1352,6 @@ public class PeacockChannel extends AutomaticsTestBase {
 		LOGGER.info("ENDING TEST CASE: TC-RDKV-STB-4019");
 
 	}
-	
-	
+
+
 }
