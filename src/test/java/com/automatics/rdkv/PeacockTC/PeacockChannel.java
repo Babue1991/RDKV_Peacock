91a7e6e7450e6a99849f3e22a0526d6609d5d72b
Babue1991 <babufsp1@gmail.com>
2023-03-21 11:23:09 +0530

    code changes

--- src/test/java/com/automatics/rdkv/PeacockTC/PeacockChannel.java
+++ src/test/java/com/automatics/rdkv/PeacockTC/PeacockChannel.java
@@ -267,7 +267,7 @@ public class PeacockChannel extends AutomaticsTestBase {
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
 
-			Thread.sleep(3000);
+			Thread.sleep(5000);
 
 			LOGGER.info("Click Xfinity OK button ");
 			CommonMethods.execCommandIcon(RemoteKeyContstants.OK_BUTTON);
